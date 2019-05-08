22e119dc92b0e707222eb521a315f645e7d82063
github username <chao.alexander.123@gmail.com>
2019-05-08 12:36:13 -0400

    create Deck component

---
+++ src/components/Deck.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Deck extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cards: []
+    }
+  }
+
+  render() {
+    return(
+      <h1>Deck</h1>
+    )
+  }
+}
+
+export default Deck
---
+++ src/containers/DeckContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class DeckContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      cards: []
+    }
+  }
+
+  render() {
+    return(
+      <h1>Deck Container</h1>
+    )
+  }
+}
+
+export default DeckContainer
