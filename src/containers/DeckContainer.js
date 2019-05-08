0aae536c358efc4ee41056445b7ee90769329e35
github username <chao.alexander.123@gmail.com>
2019-05-08 13:05:29 -0400

    make fetch request to 'api/v1/decks' in componentDidMount

--- src/containers/DeckContainer.js
+++ src/containers/DeckContainer.js
@@ -9,13 +9,24 @@ class DeckContainer extends Component {
   }
 
   render() {
+    let decks = this.state.decks.map(deck => {
+      return <h1>{deck.id}</h1>
+    })
+
     return(
-      <h1>Deck Container</h1>
+      <div>
+        <h1>Deck Container</h1>
+        {decks}
+      </div>
     )
   }
 
   componentDidMount() {
-    
+    fetch('http://localhost:3000/api/v1/decks')
+    .then(response => response.json())
+    .then(json => this.setState({
+      decks: [...json]
+    }))
   }
 }
 
