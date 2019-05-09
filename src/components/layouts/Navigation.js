399bed804a99c3403263a9cc180bb17bf0b79fd5
github username <chao.alexander.123@gmail.com>
2019-05-09 17:40:29 -0400

    create HeaderContainer component to encapsulate Navigation

---
+++ src/components/layouts/Navigation.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react';
+
+class Navigation extends Component {
+  render() {
+    return(
+      <Fragment>
+      </Fragment>
+    )
+  }
+}
+
+export default Navigation
---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react'
+import Navigation from '../components/layouts/Navigation'
+
+class HeaderContainer extends Component {
+  render() {
+    return(
+      <Fragment>
+        <Navigation/>
+      </Fragment>
+    )
+  }
+}
+
+export default HeaderContainer
