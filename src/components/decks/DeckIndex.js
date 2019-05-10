ac50b62c30a0cc7a3eb602528782bb9b80bbacaa
github username <chao.alexander.123@gmail.com>
2019-05-10 11:06:30 -0400

    contain the DeckDisplay components inside of the Paper and Grid components for responsiveness

--- src/components/decks/DeckIndex.js
+++ src/components/decks/DeckIndex.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react'
 import DeckHeader from './elements/DeckHeader'
 import DeckDisplay from './DeckDisplay'
 import DeckIndexBody from './elements/DeckIndexBody'
+import DeckIndexWrapper from './elements/DeckIndexWrapper'
+import { Grid, Paper, withStyles } from '@material-ui/core'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    overflow: 'hidden',
+    padding: `0 ${theme.spacing.unit * 3}px`,
+  },
+  paper: {
+    maxWidth: 400,
+    margin: `${theme.spacing.unit}px auto`,
+    padding: theme.spacing.unit * 2,
+  },
+})
 
 class DeckIndex extends Component {
   constructor() {
@@ -21,22 +36,34 @@ class DeckIndex extends Component {
     .then(decks => this.setState({decks: decks}))
   }
 
-  displayDecks = (deck, index) => <DeckDisplay key={index} deck={deck}/>
+  displayDecks = (deck, index) => {
+    const { classes } = this.props
+    return(<Paper className={classes.paper}>
+        <Grid container wrap="nowrap" spacing={16}>
+          <Grid item xs zeroMinWidth>
+            <DeckDisplay key={index} deck={deck}/>
+          </Grid>
+        </Grid>
+      </Paper>
+  )}
 
   componentDidMount() {
     this.loadDecks()
   }
 
   render() {
+    const { classes } = this.props
     let decks = this.state.decks.map(this.displayDecks)
 
     return(
       <DeckIndexBody>
-        <DeckHeader>Decks Index</DeckHeader>
-        {decks}
+        <DeckIndexWrapper className={classes.root}>
+          <DeckHeader>Decks Index</DeckHeader>
+          {decks}
+        </DeckIndexWrapper>
       </DeckIndexBody>
     )
   }
 }
 
-export default DeckIndex
+export default withStyles(styles)(DeckIndex)
